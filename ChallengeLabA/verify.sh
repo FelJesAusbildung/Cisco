e8825bc144363e217d14b3db763d7a0252183298
FelixJeschonneck <71749278+FelixJeschonneck@users.noreply.github.com>
2020-09-24 12:59:05 +0200

    Update verify.sh

--- ChallengeLabA/verify.sh
+++ ChallengeLabA/verify.sh
@@ -6,6 +6,6 @@ groups engineering-admin engineering-user1 engineering-user2
 groups sales-admin sales-user1 sales-user2
 groups is-admin is-user1 is-user2
 
-stat /Engineering/ /Sales/ /IS/|egrep “File:|Access \(”|cat
+stat /Engineering/ /Sales/ /IS/|egrep "File:|Access \("|cat
 
 stat /Engineering/document.txt /Sales/document.txt /IS/document.txt|egrep "File:|Access: \("|cat
