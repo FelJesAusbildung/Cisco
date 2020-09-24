c10c0aa4b5a976eba19d8e2bae9367422814ec49
FelixJeschonneck <71749278+FelixJeschonneck@users.noreply.github.com>
2020-09-24 13:16:02 +0200

    Update verify.sh

--- ChallengeLabA/verify.sh
+++ ChallengeLabA/verify.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-echo "Groups:" >> verify.log
+> verify.log
+echo "~Groups:" >> verify.log
 getent group | tail -3 >> verify.log
-echo "Users:" >> verify.log
+echo "~Users:" >> verify.log
 getent passwd | tail -9 >> verify.log
 
-echo "Groups of Users:" >> verify.log
+echo "~Groups of Users:" >> verify.log
 groups engineering-admin engineering-user1 engineering-user2 >> verify.log
 groups sales-admin sales-user1 sales-user2 >> verify.log
 groups is-admin is-user1 is-user2 >> verify.log
 
-echo "Directories:" >> verify.log
+echo "~Directories:" >> verify.log
 stat /Engineering/ /Sales/ /IS/|egrep "File:|Access: \("|cat >> verify.log
 
-echo "Confidential Documents" >> verify.log
+echo "~Confidential Documents" >> verify.log
 stat /Engineering/document.txt /Sales/document.txt /IS/document.txt|egrep "File:|Access: \("|cat >> verify.log
 echo "verify.log has been created and all info has been documented there."
+echo "verify.log will open in 5 seconds."
+sleep 5
+less verify.log
