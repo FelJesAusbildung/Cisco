068ca62a685bb19b4149ecaacbda01f44f6fffb5
FelixJeschonneck <71749278+FelixJeschonneck@users.noreply.github.com>
2020-09-24 13:04:34 +0200

    Update verify.sh

--- ChallengeLabA/verify.sh
+++ ChallengeLabA/verify.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-getent group | tail -3
-getent passwd | tail -9
+echo "Groups:" > verify.log
+getent group | tail -3 > verify.log
+echo "Users:" > verify.log
+getent passwd | tail -9 > verify.log
 
-groups engineering-admin engineering-user1 engineering-user2
-groups sales-admin sales-user1 sales-user2
-groups is-admin is-user1 is-user2
+echo "Groups of Users:" > verify.log
+groups engineering-admin engineering-user1 engineering-user2 > verify.log
+groups sales-admin sales-user1 sales-user2 > verify.log
+groups is-admin is-user1 is-user2 > verify.log
 
-stat /Engineering/ /Sales/ /IS/|egrep "File:|Access \("|cat
+echo "Directories:" > verify.log
+stat /Engineering/ /Sales/ /IS/|egrep "File:|Access: \("|cat > verify.log
 
-stat /Engineering/document.txt /Sales/document.txt /IS/document.txt|egrep "File:|Access: \("|cat
+echo "Confidential Documents" > verify.log
+stat /Engineering/document.txt /Sales/document.txt /IS/document.txt|egrep "File:|Access: \("|cat > verify.log
